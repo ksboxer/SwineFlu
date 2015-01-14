be6ea48df74b1f5cad606e6b82287af554b683d6
Toby Kahan <tobyk100@gmail.com>
2015-01-15 01:29:58 +0200

    Added DLL injection code from last week.

---
+++ Common/Common.h
@@ -0,0 +1,21 @@
+#ifndef INJECTEE_H_
+#define IJNECTEE_H_
+#include <windows.h>
+#include <algorithm>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sstream>
+#include <string>
+#include <tchar.h>
+#include <psapi.h>
+namespace InjecteeFuncs {
+	class MyInjecteeFuncs {
+	public:
+		static void inject(int process_id);
+		static int isProcess(DWORD processID, TCHAR *process_name);
+		static int compare(const void * a, const void * b);
+		static int getNextProcessID(int current_process_id);
+	};
+}
+#endif
